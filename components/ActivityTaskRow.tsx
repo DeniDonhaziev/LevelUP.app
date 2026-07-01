9b7345cdcf76e30c669414e77b99deb939f458a1
DeniDonhaziev <blackskydonhaziev@gmail.com>
2026-07-01 18:51:10 +0300

    style: чёрный фон и карточки вместо серого (лайм-акцент сохранён)

--- components/ActivityTaskRow.tsx
+++ components/ActivityTaskRow.tsx
@@ -86,6 +86,6 @@ const styles = StyleSheet.create({
   },
   nameWrap: { flex: 1 },
   name: { fontSize: 15, fontFamily: 'Inter_500Medium' },
-  nameDone: { textDecorationLine: 'line-through', color: '#9A9EA8' },
+  nameDone: { textDecorationLine: 'line-through', color: '#8A8A8A' },
   deleteBtn: { padding: 4 },
 });
